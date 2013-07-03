e848d68fadec19534d102085e9c18c18eccbcd8c
Michal Pokorný <pok@rny.cz>
2013-07-03 15:39:29 +0200

    split tasks

---
+++ lib/kgr/tasks/prepare-data.rb
@@ -0,0 +1,9 @@
+module KGR
+	module Tasks
+		module PrepareData
+			def self.run(argv)
+				puts "TODO: prepare KGR data, argv = #{argv.inspect}"
+			end
+		end
+	end
+end
---
+++ lib/kgr/tasks/train.rb
@@ -0,0 +1,9 @@
+module KGR
+	module Tasks
+		module Train
+			def self.run(argv)
+				puts "TODO: train KGR data, argv = #{argv.inspect}"
+			end
+		end
+	end
+end
