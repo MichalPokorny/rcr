e38539f511fc702180bcb156c5f961d9ae418099
Michal Pokorný <pok@rny.cz>
2013-07-06 01:58:37 +0200

    first segmentation: based on continuous blocks

--- lib/kgr/data/segmentation-box.rb
+++ lib/kgr/data/segmentation-box.rb
@@ -1,15 +1,16 @@
 module KGR
 	module Data
 		class SegmentationBox
-			def initialize(image, x, y, width, height)
+			def initialize(x, y, image)
 				@x = x
 				@y = y
-				@width = width
-				@height = height
-				@image = image.crop(@x, @y, @width, @height)
+				@image = image
 			end
 
-			attr_reader :x, :y, :width, :height, :image
+			def width; @image.width; end
+			def height; @image.height; end
+
+			attr_reader :x, :y, :image
 
 			def to_s
 				"Box<(#{x};#{y}) #{width}x#{height}>"
