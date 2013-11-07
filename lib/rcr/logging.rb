6e22558174ff54fa5f5fd80130d12623728619fb
Michal Pokorný <pok@rny.cz>
2013-11-07 19:16:27 +0100

    fixed logging

--- lib/rcr/logging.rb
+++ lib/rcr/logging.rb
@@ -1,16 +1,41 @@
+require 'rcr'
+require 'rcr/config'
+
 module RCR::Logging
+	ENABLED_BY_DEFAULT = RCR::Config.logging_enabled
+
+	def self.log_line(*args)
+		puts "[log] #{args.join}"
+	end
+
 	def self.included(klass)
 		klass.instance_eval do
+			define_method :logging_enabled? do
+				if defined?(@enable_logging)
+					@enable_logging
+				else
+					ENABLED_BY_DEFAULT
+				end
+			end
+
 			define_method :log do |*args|
-				puts("LOG:", *args) if defined?(@enable_logging) && @enable_logging
+			  RCR::Logging.log_line(*args) if logging_enabled?
 			end
 
 			attr_accessor :enable_logging
 		end
 
 		class << klass
+			define_method :logging_enabled? do
+				if defined?(@enable_logging)
+					@enable_logging
+				else
+					ENABLED_BY_DEFAULT
+				end
+			end
+
 			define_method :log do |*args|
-				puts("LOG:", *args) if defined?(@enable_logging) && @enable_logging
+				RCR::Logging.log_line(*args) if logging_enabled?
 			end
 		end
 	end
