69e6888c6b486d6c982d6e27541f4006d48339f4
Michal Pokorný <pok@rny.cz>
2013-11-11 20:44:18 +0100

    work

--- lib/rcr/easy.rb
+++ lib/rcr/easy.rb
@@ -1,13 +1,23 @@
+require 'rcr/config'
 require 'rcr/letter-classifier/neural'
 
 module RCR
-	def build_letter_classifier
+	def build_letter_classifier(opts = {})
+		config =
+			if opts.key?(:config)
+				opts[:config]
+			elsif opts.key?(:config_file)
+				RCR::Config.load_from_file(opts[:config_file])
+			else
+				RCR::Config.instance
+			end
+
 		require 'rcr/letter-classifier/neural'
 		# TODO: load the classifier we should actually load!
-		RCR::LetterClassifier::Neural.load(RCR::Config.letter_classifier_path)
+		RCR::LetterClassifier::Neural.load(config.letter_classifier_path)
 	end
 
-	def build_language_model
+	def build_language_model(opts = {})
 		# TODO: load the language model we should actually load!
 	
 		# require 'rcr/markov-chain-model'
@@ -16,7 +26,7 @@ module RCR
 		nil
 	end
 
-	def build_word_segmentator
+	def build_word_segmentator(opts = {})
 		require 'rcr/word-segmentator/heuristic-oversegmentation'
 		require 'rcr/heuristic-oversegmenter/local-minima'
 		# TODO: load the segmentator we should actually load!
